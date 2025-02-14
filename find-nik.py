63a3a55b89746aeda5f4ed1d5cb8893feccb8257
adisp7 <adinovasp@gmail.com>
2025-02-14 13:48:36 +0700

    Testing Bio

--- find-nik.py
+++ find-nik.py
@@ -2,11 +2,9 @@ import cv2
 import numpy as np
 import os
 import pytesseract
-goodResultLatest = 0
-goodResultOld = 0
 
 
-def nikFinder(image, img_gray, filename, dir, goodResultLatest, goodResultOld):
+def nikFinder(image, img_gray, filename, dir, grl = 0, gro = 0):
     rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
     tophat = cv2.morphologyEx(img_gray, cv2.MORPH_TOPHAT, rectKernel)
 
@@ -34,30 +32,44 @@ def nikFinder(image, img_gray, filename, dir, goodResultLatest, goodResultOld):
         # if ar > 3:
         # if (w > 40 ) and (h > 10 and h < 20):
         if h > 10 and w > 100 and x < 300 and x > 4:
-            crop_img = image[y-4:y+h+4, x-4:x+w+4]
-            gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-            blur = cv2.GaussianBlur(gray, (3, 3), 0)
-            thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            try:
+                crop_img = image[y-4:y+h+4, x-4:x+w+4]
+                gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+                blur = cv2.GaussianBlur(gray, (3, 3), 0)
+                # Testing Biodata
+                thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_TRUNC + cv2.THRESH_OTSU)[1]
+
+                # Testing NIK
+                # thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+                # Testing Biodata
+                aocra_old = pytesseract.image_to_string(thresh, lang="Arialv2.4", config='--psm 4 --oem 3')
+                aocra_latest = pytesseract.image_to_string(thresh, lang="Arialv2.5", config='--psm 4 --oem 3')
+                
+                # Testing NIK
+                # aocra_old = pytesseract.image_to_string(thresh, lang="OCRA-NEW", config='--psm 7 --oem 3')
+                # aocra_latest = pytesseract.image_to_string(thresh, lang="OCRA-11-02-2024v1.4", config='--psm 7 --oem 3')
+                whitespace_count = aocra_latest.count(' ')
+                
+                newfilename = os.path.splitext(filename)[0]
+                newfilename = newfilename.split("images/"+dir+"\\")[-1]
+                newfilename = f"{dir}/{dir}-{newfilename}"
+                if len(aocra_latest) > 11 and len(aocra_latest) < 20 and whitespace_count <= 0:
+                    # writeToFile(aocra_latest, "ocred/"+newfilename, thresh, dir)
+                    if resultChecker(aocra_latest, "ground_truth/"+newfilename):
+                        grl += 1
+                if len(aocra_old) > 11 and len(aocra_old) < 20 and whitespace_count <= 0:
+                    # writeToFile(aocra_latest, "ocred/"+newfilename, thresh, dir)
+                    if resultChecker(aocra_old, "ground_truth/"+newfilename):
+                        gro += 1
+                    return grl, gro
             
-            aocra_old = pytesseract.image_to_string(thresh, lang="OCRA-NEW", config='--psm 7 --oem 3')
-            aocra_latest = pytesseract.image_to_string(thresh, lang="OCRA-NEW_1", config='--psm 7 --oem 3')
-            whitespace_count = aocra_latest.count(' ')
-            
-            newfilename = os.path.splitext(filename)[0]
-            newfilename = newfilename.split("images/"+dir+"\\")[-1]
-            newfilename = f"{dir}/{dir}-{newfilename}"
-            if len(aocra_latest) > 11 and len(aocra_latest) < 20 and whitespace_count <= 0:
-                # writeToFile(aocra_latest, "ocred/"+newfilename, thresh, dir)
-                if resultChecker(aocra_latest, "ground_truth/"+newfilename):
-                    goodResultLatest += 1
-            
-            if len(aocra_old) > 11 and len(aocra_old) < 20 and whitespace_count <= 0:
-                # writeToFile(aocra_old, "ocred/"+newfilename, thresh, dir)
-                if resultChecker(aocra_old, "ground_truth/"+newfilename):
-                    goodResultOld += 1
-            
-                    
-    return goodResultLatest, goodResultOld
+                # if len(aocra_old) > 11 and len(aocra_old) < 20 and whitespace_count <= 0:
+                # # writeToFile(aocra_old, "ocred/"+newfilename, thresh, dir)
+                #     if resultChecker(aocra_old, "ground_truth/"+newfilename):
+                #         gro += 1
+            except e:
+                print("err " + e)
                            
 def writeToFile(ocrResult, filename, img):
     cv2.imwrite(filename+".png", img)
@@ -82,20 +94,23 @@ def listFiles(dir):
     return r
             
 if __name__ == '__main__':
-    dirs = ["1", "2", "3"]
+    dirs = ["3"]
+    goodResultLatest = 0
+    goodResultOld = 0
     for dir in dirs:
         files = listFiles(dir)
         for filename in files:
             try:
-                npimg = np.fromfile(filename)
+                npimg = np.fromfile(filename, dtype='uint8')
                 image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
                 image = cv2.resize(image, (50 * 16, 500))
                 img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
                 rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 7))
                 blackhat = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, rectKernel)
                 goodResultLatest, goodResultOld = nikFinder(image, blackhat, filename, dir, goodResultLatest, goodResultOld)
+
             except Exception as e:
                 continue
-    print(f"Good Result Latest: {goodResultLatest}")
-    print(f"Good Result Old: {goodResultOld}")
+    print(f"Good Result Latest: {str(goodResultLatest)}")
+    print(f"Good Result Old: {str(goodResultOld)}")
     
